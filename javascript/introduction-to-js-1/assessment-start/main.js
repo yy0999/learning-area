d30ad4d5a5db30524b0be9117f54b92ae4770f64
YY <1920566903@qq.com>
2023-07-04 10:02:49 +0800

    完成笑話生成器

--- javascript/introduction-to-js-1/assessment-start/main.js
+++ javascript/introduction-to-js-1/assessment-start/main.js
@@ -7,34 +7,40 @@ const story = document.querySelector('.story');
 function randomValueFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
-let storyText ='今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。';
-let insertX = ['怪兽威利','大老爹','圣诞老人'];
-let insertY = ['肯德基','迪士尼乐园','白宫'];
-let insertZ = ['自燃了','在人行道化成了一坨泥','变成一条鼻涕虫爬走了'];
+let storyText = '今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。';
+let insertX = ['怪兽威利', '大老爹', '圣诞老人'];
+let insertY = ['肯德基', '迪士尼乐园', '白宫'];
+let insertZ = ['自燃了', '在人行道化成了一坨泥', '变成一条鼻涕虫爬走了'];
 
 // 事件监听器和未完成的函数定义
 
 randomize.addEventListener('click', result);
 
 function result() {
-    let newStory = storyText;
-    let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
-    let zItem = randomValueFromArray(insertY);
-    newStory = newStory.replace(":inserta:",xItem);
-    newStory = newStory.replace(":insertb:",yItem);
-    newStory = newStory.replace(":insertc:",zItem);
-  if(customName.value !== '') {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertY);
+
+  newStory = newStory.replace(":inserta:", xItem);
+  newStory = newStory.replace(":insertb:", yItem);
+  newStory = newStory.replace(":insertc:", zItem);
+
+  if (customName.value !== '') {
     let name = customName.value;
-
+    newStory.replace("李雷", name);
   }
 
-  if(document.getElementById("american").checked) {
-    let weight = Math.round(300);
-    let temperature = Math.round(94);
-
+  if (document.getElementById("american").checked) {
+    const weight = Math.round(130 * 2.20462) + ' 磅';
+    const temperature = Math.round(34 * 9 / 5 + 32) + ' 华氏度';
+    newStory = newStory.replace('34 摄氏度', temperature);
+    newStory = newStory.replace('130 公斤', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
+  //測試用代碼，檢測js引用是否生效
+  document.querySelector('html').style.backgroundColor = 'red';
+
 }
