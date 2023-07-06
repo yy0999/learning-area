7e61aeefa1887e17a3c913bf51822dfb1ca2805e
YY <1920566903@qq.com>
2023-07-06 19:05:34 +0800

    增加功能-待完成

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -8,20 +8,20 @@ const height = canvas.height = window.innerHeight;
 
 // 生成随机数的函数
 
-function random(min,max) {
+function random(min, max) {
   const num = Math.floor(Math.random() * (max - min)) + min;
   return num;
-}
+};
 
 // 生成随机颜色值的函数
 
 function randomColor() {
   const color = 'rgb(' +
-                random(0, 255) + ',' +
-                random(0, 255) + ',' +
-                random(0, 255) + ')';
+    random(0, 255) + ',' +
+    random(0, 255) + ',' +
+    random(0, 255) + ')';
   return color;
-}
+};
 
 // 定义 Shape 构造器
 
@@ -31,7 +31,7 @@ function Shape(x, y, velX, velY, exists) {
   this.velX = velX;
   this.velY = velY;
   this.exists = exists;
-}
+};
 
 // 定义 Ball 构造器，继承自 Shape
 
@@ -40,46 +40,78 @@ function Ball(x, y, velX, velY, exists, color, size) {
 
   this.color = color;
   this.size = size;
-}
+};
 Ball.prototype = Object.create(Shape.prototype);
 Ball.prototype.constructor = Ball;
 
-function EvilCircle(x, y, velX, velY, exists,co){
-  Shape.call(this,x,y,20,20,exists);
-}
+function EvilCircle(x, y, velX, velY, exists, co) {
+  Shape.call(this, x, y, 20, 20, exists);
+};
 EvilCircle().prototype = Object.create(Shape.prototype);
 EvilCircle().prototype.constructor = EvilCircle;
 
-EvilCircle.prototype.draw = function(){
+EvilCircle.prototype.draw = function () {
   ctx.beginPath();
   ctx.lineWidth = 3;
   ctx.strokeStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.stroke();
-}
+};
 
-EvilCircle.prototype.checkBounds() = function(){
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
+EvilCircle.prototype.checkBounds() = function () {
+  if ((this.x + this.size) >= width) {
+    this.x -= this.size;
   }
 
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
+  if ((this.x - this.size) <= 0) {
+    this.x += this.size;
   }
 
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
+  if ((this.y + this.size) >= height) {
+    this.y -= this.size;
   }
 
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
+  if ((this.y - this.size) <= 0) {
+    this.y += this.size;
   }
+};
 
+EvilCircle.prototype.setControls() = function () {
+  window.onkeydown = e => {
+    switch (e.key) {
+      case 'a':
+        this.x -= this.velX;
+        break;
+      case 'd':
+        this.x += this.velX;
+        break;
+      case 'w':
+        this.y -= this.velY;
+        break;
+      case 's':
+        this.y += this.velY;
+        break;
+    }
+  };
+};
+
+EvilCircle.prototype.collisionDetect() = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (this !== undefined) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = randomColor();
+      }
+    }
+  }
 }
 
 // 定义彩球绘制函数
 
-Ball.prototype.draw = function() {
+Ball.prototype.draw = function () {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
@@ -88,20 +120,20 @@ Ball.prototype.draw = function() {
 
 // 定义彩球更新函数
 
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
 
-  if((this.x - this.size) <= 0) {
+  if ((this.x - this.size) <= 0) {
     this.velX = -(this.velX);
   }
 
-  if((this.y + this.size) >= height) {
+  if ((this.y + this.size) >= height) {
     this.velY = -(this.velY);
   }
 
-  if((this.y - this.size) <= 0) {
+  if ((this.y - this.size) <= 0) {
     this.velY = -(this.velY);
   }
 
@@ -111,9 +143,9 @@ Ball.prototype.update = function() {
 
 // 定义碰撞检测函数
 
-Ball.prototype.collisionDetect = function() {
-  for(let j = 0; j < balls.length; j++) {
-    if(this !== balls[j]) {
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (this !== balls[j]) {
       const dx = this.x - balls[j].x;
       const dy = this.y - balls[j].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
@@ -129,8 +161,8 @@ Ball.prototype.collisionDetect = function() {
 
 let balls = [];
 
-while(balls.length < 25) {
-  const size = random(10,20);
+while (balls.length < 25) {
+  const size = random(10, 20);
   let ball = new Ball(
     // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
     random(0 + size, width - size),
@@ -148,9 +180,9 @@ while(balls.length < 25) {
 
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
+  ctx.fillRect(0, 0, width, height);
 
-  for(let i = 0; i < balls.length; i++) {
+  for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
     balls[i].collisionDetect();
